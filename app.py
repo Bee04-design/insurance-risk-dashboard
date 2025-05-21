30463d27ff5deea6d404816fc7ab95f3a646dc50
Bee04-design <bhekiwed2@gmail.com>
2025-05-21 03:37:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
